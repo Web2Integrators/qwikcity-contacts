7bcc0b3a6116bd54ba2528213f123ebc2e4157b6
harshmangalam <harshdev8218@gmail.com>
2023-04-09 07:59:28 +0530

    added tailwind

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,34 @@
+import { component$ } from "@builder.io/qwik";
 import { type DocumentHead } from "@builder.io/qwik-city";
 
-<section class="min-h-screen grid place-items-center">
-  <div class="max-w-md w-full mx-auto text-center flex flex-col space-y-4">
-    <img
-      src="https://kit.svelte.dev/favicon.png"
-      class="w-16 h-16 mx-auto"
-      loading="lazy"
-      alt=""
-    />
+export default component$(() => {
+  return (
+    <section class="min-h-screen grid place-items-center">
+      <div class="max-w-md w-full mx-auto text-center flex flex-col space-y-4">
+        <img
+          src="https://kit.svelte.dev/favicon.png"
+          class="w-16 h-16 mx-auto"
+          loading="lazy"
+          alt=""
+        />
 
-    <div class="text-gray-500">
-      <p>This is a demo for Sveltekit</p>
-      <p>
-        Check out the docs at{" "}
-        <a href="https://kit.svelte.dev/" target="_blank" class="text-blue-500">
-          https://kit.svelte.dev
-        </a>
-      </p>
-    </div>
-  </div>
-</section>;
+        <div class="text-gray-500">
+          <p>This is a demo for Sveltekit</p>
+          <p>
+            Check out the docs at{" "}
+            <a
+              href="https://kit.svelte.dev/"
+              target="_blank"
+              class="text-blue-500"
+            >
+              https://kit.svelte.dev
+            </a>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+});
 
 export const head: DocumentHead = {
   title: "Contacts | Qwikcity",

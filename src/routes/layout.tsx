c8141cf63e11dbd1adf8a5ed6987bc0499053481
harshmangalam <harshdev8218@gmail.com>
2023-04-09 07:34:31 +0530

    added data fetching for contacts

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,6 +1,38 @@
 import { component$, Slot } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
+import { prisma } from "~/lib/prisma";
 
+export const useContacts = routeLoader$(async ({ url }) => {
+  const search = url.searchParams.get("search");
+  const contacts = await prisma.contact.findMany({
+    where: search?.trim().length
+      ? {
+          OR: [
+            {
+              firstName: {
+                contains: search,
+              },
+            },
+            {
+              lastName: {
+                contains: search,
+              },
+            },
+          ],
+        }
+      : undefined,
+    select: {
+      id: true,
+      firstName: true,
+      lastName: true,
+    },
+  });
+  return {
+    contacts,
+  };
+});
 export default component$(() => {
+  const contactsSignal = useContacts();
   return (
     <div>
       <main>
